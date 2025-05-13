742b66126fc69fdd6b88e96849e836c59d0c29fe
Adithyan <adhithyapraveen@gmail.com>
2025-05-13 19:57:35 +0530

    added navbar click state

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png';
+import {useState} from 'react';
+
 const Navbar = () => {
+    const [menu,setMenu] = useState("shop");
+
 
     return (
         <div className = "navbar">
@@ -12,10 +16,10 @@ const Navbar = () => {
             </div>
 
             <ul className = "nav-menu">
-                <li>Shop <hr/> </li>
-                <li>Men </li>
-                <li>Women </li>
-                <li>Kids </li>
+                <li onClick = { () => {setMenu("shop")} } > Shop {menu === "shop" ? <hr/>:<></>} </li>
+                <li onClick = { () => {setMenu("men") } }>Men {menu === "men" ? <hr/> : <> </> } </li>
+                <li onClick = { () => {setMenu("women")} } >Women {menu === "women" ? <hr/> : <> </> } </li>
+                <li onClick = { () => {setMenu("kids")} }>Kids {menu === "kids" ?  <hr/> : <></>} </li>
             </ul>
 
             <div className = "nav-login-cart" >
